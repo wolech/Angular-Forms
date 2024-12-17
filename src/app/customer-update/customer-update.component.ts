d11a862548d396ff687ca013064b89721eb52416
wolech <wojciech.lech@hyland.com>
2024-12-17 08:59:23 +0100

    ccs class addition

--- src/app/customer-update/customer-update.component.ts
+++ src/app/customer-update/customer-update.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatInputModule} from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-customer-update',
   templateUrl: './customer-update.component.html',
-  styleUrls: ['./customer-update.component.css']
+  styleUrls: ['./customer-update.component.css'],
+  imports: [CommonModule, FormsModule, MatDatepickerModule, MatInputModule, MatFormFieldModule, MatCardModule]
 })
+
 export class CustomerUpdateComponent {
   customer = {
     accountNumber: '',
@@ -15,6 +23,7 @@ export class CustomerUpdateComponent {
     newAdminEmail: '',
     date: ''
   };
+user: any;
 
   onUpdate() {
     console.log('Updated Customer Data:', this.customer);
