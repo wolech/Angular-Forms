85a9b1a71dca13a43b91429dc79e4809af25a119
wolech <wojciech.lech@hyland.com>
2025-01-03 12:29:21 +0100

    endpage  added

---
+++ src/app/endpage/endpage.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-endpage',
+  imports: [],
+  templateUrl: './endpage.component.html',
+  styleUrl: './endpage.component.css'
+})
+export class EndpageComponent {
+
+}
