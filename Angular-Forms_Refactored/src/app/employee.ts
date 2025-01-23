ed49fe46d99ac5895f5b446d01a17e0a0a1b2c3a
kaushiknatua <75721842+kaushiknatua12345@users.noreply.github.com>
2025-01-23 12:54:49 +0530

    Delete Angular-Forms_Refactored directory

--- Angular-Forms_Refactored/src/app/employee.ts
+++
@@ -1,9 +0,0 @@
-export class Employee {
-    fisrtName?: string;
-    lastName?: string;
-    username?: string;
-    password?: string;
-    email?: string;
-    phone?: string;
-    dateofBirth?: string;
-}
